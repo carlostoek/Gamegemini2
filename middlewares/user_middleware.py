066373e976a2870a02c9087c8645be5b63f37866
carlostoek <carlostoek@gmail.com>
2025-06-10 15:45:25 -0600

    Update user_middleware.py

--- middlewares/user_middleware.py
+++ middlewares/user_middleware.py
@@ -4,11 +4,12 @@ from aiogram import BaseMiddleware
 from aiogram.types import Message, CallbackQuery
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
+from sqlalchemy.sql import func # <--- ¡¡¡ESTA LÍNEA ES CRÍTICA Y DEBE ESTAR AQUÍ!!!
 from database.models.user import User
 from utils.logger import logger
 from database.models.badge import INITIAL_BADGES
 from config.settings import Settings
-import json # <--- Asegúrate de que json esté importado aquí
+import json
 
 class UserMiddleware(BaseMiddleware):
     def __init__(self, settings: Settings):
@@ -44,14 +45,15 @@ class UserMiddleware(BaseMiddleware):
                 last_name=telegram_user.last_name,
                 points=0,
                 level_id=1,
-                is_admin=telegram_user.id in self.settings.ADMIN_IDS
+                is_admin=telegram_user.id in self.settings.ADMIN_IDS,
+                last_interaction_at=func.now(), # Inicializar para nuevos usuarios
+                interactions_count=1 # Primera interacción
             )
 
             # Asignar la insignia "Nuevo Suscriptor Íntimo"
             new_user_badge = next((badge for badge in INITIAL_BADGES if badge['id'] == 1), None)
             if new_user_badge:
-                # Si el usuario es nuevo, badges_json debería ser "[]", pero lo aseguramos
-                badges = json.loads(new_user.badges_json if new_user.badges_json else "[]") # <--- ¡CAMBIO AQUÍ!
+                badges = json.loads(new_user.badges_json if new_user.badges_json else "[]")
                 if new_user_badge['name'] not in [b['name'] for b in badges]:
                     badges.append({"id": new_user_badge['id'], "name": new_user_badge['name'], "description": new_user_badge['description']})
                     new_user.badges_json = json.dumps(badges)
@@ -63,13 +65,9 @@ class UserMiddleware(BaseMiddleware):
             user = new_user
             logger.info(f"Nuevo usuario registrado: {user.username or user.first_name} (ID: {user.id})")
         else:
-            # Si el usuario ya existe, asegurar que badges_json no sea None al leerlo
-            # CAMBIO AQUÍ: Asegurar que badges_json sea una cadena válida antes de cargarla
+            # Si el usuario ya existe, asegurar que badges_json no sea None
             user.badges_json = user.badges_json if user.badges_json is not None else "[]"
-            # No necesitamos cargarla aquí, solo asegurarnos de que tenga un valor por defecto si es None
-            # El código de arriba ya maneja la asignación de la insignia si el usuario es nuevo.
-            # Aquí podríamos querer actualizar 'last_interaction_at' e 'interactions_count'
-            user.last_interaction_at = func.now()
+            user.last_interaction_at = func.now() # Ahora 'func' estará definido
             user.interactions_count += 1
             await session.commit() # ¡Importante guardar los cambios!
 
